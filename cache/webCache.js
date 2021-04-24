42b256dc529b75cb0ebbb56fc4d959fd12325ad5
Duc Nguyen <nahz3112@gmail.com>
2021-04-24 16:46:18 -0400

    UPDATE: Added error interceptors

--- cache/webCache.js
+++ cache/webCache.js
@@ -2,18 +2,29 @@ const isBrowserCompatible = () => { return window.indexedDB };
 
 class CacheSimplified {
     constructor(dbname) {
+        this.dbname = dbname;
+        if(this.dbExisted()) 
+            throw new Error("Database already existed. Considered updating or deleting the database");
+        if (!window.indexedDB)
+            throw new Error("Your browser does not support IndexedDB")
         this.version = 1;
-        this.cacheDB = window.indexedDB.open(dbname, this.version);
+        this.cacheDB = window.indexedDB.open(this.dbname, this.version);
     }
 
     getKeyPath(data) {
         if (!data && typeof data !== 'object') return [""]
         return Object.keys(data);
     }
-    
+
     // Check if db exist
     dbExisted() {
-
+        var dbExists = true;
+        var request = window.indexedDB.open(this.dbname);
+        request.onupgradeneeded = e => {
+            e.target.transaction.abort();
+            dbExists = false;
+        }
+        return dbExists
     }
 
     addTable(table, data) {
